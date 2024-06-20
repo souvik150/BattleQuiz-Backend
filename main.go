448eb6d219c9210c1e07db8d82b8ab5ce6d73fd3
souvik150 <souvikmukherjee150@gmail.com>
2024-06-21 04:24:15 +0530

    fix: main.go

--- main.go
+++ main.go
@@ -1,38 +1,45 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
-	"net/url"
 
-	_ "github.com/lib/pq"
+	"github.com/gin-gonic/gin"
+
+	"github.com/souvik150/BattleQuiz-Backend/config"
+	"github.com/souvik150/BattleQuiz-Backend/database"
+	"github.com/souvik150/BattleQuiz-Backend/middlewares"
+	"github.com/souvik150/BattleQuiz-Backend/routes"
 )
 
 func main() {
-	serviceURI := ""
+	r := gin.New()
+	r.Use(middlewares.Logger())
+	r.Use(gin.Recovery())
 
-	conn, _ := url.Parse(serviceURI)
-	conn.RawQuery = "sslmode=verify-ca;sslrootcert=ca.pem"
+	config.LoadConfig()
+	log.Println("Configuration loaded successfully")
 
-	db, err := sql.Open("postgres", conn.String())
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT version()")
-	if err != nil {
-		panic(err)
+	// Connect to the database
+	if err := database.Connect(); err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	} else {
+		log.Println("Connected to the database successfully")
 	}
 
-	for rows.Next() {
-		var result string
-		err = rows.Scan(&result)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Version: %s\n", result)
+	// Connect to Redis
+	if err := database.ConnectRedis(); err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	} else {
+		log.Println("Connected to Redis successfully")
+	}
+
+	routes.RegisterRoutes(r)
+	log.Println("Routes registered successfully")
+
+	if err := r.Run(); err != nil {
+		log.Fatalf("Failed to run the server: %v", err)
+	} else {
+		log.Println("Server started successfully on port 8080")
 	}
 }
